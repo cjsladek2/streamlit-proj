9e5653f2b1a69b6e2013672ebff28bfa49502bc1
cjsladek2 <cjsladek2@gmail.com>
2026-06-20 11:48:52 -0400

    dataframe:

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title('🎈 Charlotte\'s Dope App')
 
 st.write('Hello world!')
+
+df = pd.read_csv("https://github.com/dataprofessor/data/blob/master/penguins_cleaned.csv")
+
+df
